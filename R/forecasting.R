aecd647e4aa265e3a5aa14ead759d9fa77b6f8ee
OmarAndujarL <omarabel00@hotmail.com>
2023-12-02 17:10:13 -0500

    Added new format for plots

--- R/forecasting.R
+++ R/forecasting.R
@@ -1,7 +1,6 @@
 library(forecast)
 library(tidyr)
 library(dplyr)
-#source('R/data_import.R')
 source('R/model_ts_NEW.R')
 
 #Based on the full data we got these models: hw1, hw2, m1, m2, m3, m4,m5, m6, m7, m8, r1, r2, r3, r4, r5, sub
@@ -28,31 +27,19 @@ test_data = crime_data$test_data
 m8_train_data <- Arima(train_data, order = c(1, 0, 0), seasonal = list(order = c(0, 1, 1), period = 12), include.drift = TRUE)
 # Make forecasts on the test data
 forecast_result <- forecast(m8_train_data, h = length(test_data))
-# Plot the results
-plot(forecast_result, main = "M8 ARIMA Forecast for Crime. In-Sample")
-lines(test_data, col = "black", lty = 2)  # Add the test set to the plot with a dashed line
-legend("topright", legend = c("Forecast", "Test Set"), col = c("blue", "black"), lty = c(1, 2))
 # Evaluate the model. First row is based on training data, second row is based on test data
 m8_90_accuracy = accuracy(forecast_result, test_data)
 
 # Model selection using auto.arima
 auto_m = auto.arima(train_data, ic = "bic")   #Maybe dont use autoarima, use the explicit models found on model_ts_NEW.
 forecast_result2 <- forecast(auto_m, h = length(test_data))
-
-plot(forecast_result2, main = "Auto-ARIMA Forecast for Crime. In-Sample")
-lines(test_data, col = "black", lty = 2)  # Add the test set to the plot with a dashed line
-legend("topright", legend = c("Forecast", "Test Set"), col = c("blue", "black"), lty = c(1, 2))
 # Evaluate the model
 auto_arima90_accuracy = accuracy(forecast_result2, test_data)
-
 
 ##############
 ### Holt-Winters
 hw_train = HoltWinters(train_data, seasonal = "multiplicative")
 hw_train_p<-predict(hw_train,n.ahead=length(test_data), prediction.interval = TRUE, level=.95)
-plot(hw_train, hw_train_p, main='H-W multiplicative Forecast for Crime. In-Sample')
-lines(test_data, col = "black", lty = 2)  # Add the test set to the plot with a dashed line
-legend("bottomleft", legend = c("Forecast", "Test Set", "95% CI"), col = c("red", "black",'lightblue'), lty = c(1, 2))
 hw_90_accuracy = accuracy(hw_train_p, test_data)
 
 ##################################
@@ -75,10 +62,7 @@ r5_train <- Arima(train_data, xreg=cbind(housing_train_data, unemp_train_data, c
                   order = c(0, 1, 0), seasonal = list(order = c(2, 0, 0), period = 12), include.drift = TRUE)
 
 forecast_result3 <- forecast(r5_train, h = length(test_data),xreg = cbind(housing_test_data, unemp_test_data, crime11_test_data))
-plot(forecast_result3, main = "R5 ARIMA for Crime with predictors. In Sample")
-lines(test_data, col = "black", lty = 2)  # Add the test set to the plot with a dashed line
-legend("bottomleft", legend = c("Forecast", "Test Set"), col = c("blue", "black"), lty = c(1, 2))
-# Evaluate the model (optional). First row is based on training data, second row is based on test data
+# Evaluate the model. First row is based on training data, second row is based on test data
 r5_90_accuracy = accuracy(forecast_result3, test_data)
 
 #Model accuracy
@@ -97,19 +81,38 @@ df <- data.frame(Model = rep(models, each = length(metrics)),
 # Pivot the data
 results_table <- df %>%
   pivot_wider(names_from = Metric, values_from = Value)
-
 # Performance table for test set
 print(results_table)
+
+# Set up a 2x2 layout for subplots
+par(mfrow = c(2, 2))
+# Plot the first subplot
+plot(forecast_result, main = "M8 ARIMA Forecast for Crime. In-Sample")
+lines(test_data, col = "black", lty = 2)  # Add the test set to the plot with a dashed line
+legend("bottomleft", legend = c("Forecast", "Test Set"), col = c("blue", "black"), 
+       lty = c(1, 2),cex = 0.7)  
+# Plot the second subplot
+plot(forecast_result2, main = "Auto-ARIMA Forecast for Crime. In-Sample")
+lines(test_data, col = "black", lty = 2)  # Add the test set to the plot with a dashed line
+legend("bottomleft", legend = c("Forecast", "Test Set"), col = c("blue", "black"), lty = c(1, 2),cex = 0.7)  
+# Plot the third subplot
+plot(hw_train, hw_train_p, main='H-W multiplicative Forecast for Crime. In-Sample')
+lines(test_data, col = "black", lty = 2)  # Add the test set to the plot with a dashed line
+legend("bottomleft", legend = c("Forecast", "Test Set"), col = c("red", "black"), lty = c(1, 2),cex = 0.7)  
+# Plot the fourth subplot
+plot(forecast_result3, main = "R5 ARIMA for Crime with predictors. In-Sample")
+lines(test_data, col = "black", lty = 2)  # Add the test set to the plot with a dashed line
+legend("bottomleft", legend = c("Forecast", "Test Set"), col = c("blue", "black"), lty = c(1, 2),cex = 0.7)  
+# Reset the layout to the default (1x1)
+par(mfrow = c(1, 1))
 
 
 ########
 ### Out of sample predictions using the models trained in model_ts_NEW.R
 
 year_forecast = forecast(m8, h = 12)
-plot(year_forecast, main = "M8 ARIMA Forecast for Crime. Out-of-Sample (1yr)")
 
 hw_p<-predict(hw2,n.ahead=12, prediction.interval = TRUE, level=.95)
-plot(hw2, hw_p, main='H-W multiplicative Forecast for Crime. Out-of-Sample (1yr)')
 
 #Estimate predictors using HW and then forecast
 housing_hw<-HoltWinters(housing_ts,seasonal='mult')
@@ -122,8 +125,24 @@ lag11crime_hw<-HoltWinters(lag11_crime_ts,seasonal='mult')
 lag11_crime_pred = predict(lag11crime_hw,n.ahead=12)
 
 forecast_r5 <- forecast(r5, h = 12,xreg = cbind(housing_pred, unem_pred, lag11_crime_pred))
-plot(forecast_r5, main = "R5 ARIMA Forecast for Crime with predictors. Out-of-Sample (1yr).")
 
+
+# Set up a 2x2 layout for subplots
+par(mfrow = c(2, 2))
+
+# Plot the first subplot
+plot(year_forecast, main = "M8 ARIMA Forecast for Crime. OoS (1yr)")
+
+# Plot the second subplot
+plot(hw2, hw_p, main='H-W mult Forecast for Crime. OoS (1yr)')
+
+# Plot the third subplot
+plot(forecast_r5, main = "R5 forecast for Crime with preds. OoS (1yr)") 
+
+# Plot the fourth subplot 
+
+# Reset the layout to the default (1x1)
+par(mfrow = c(1, 1))
 
 
 
